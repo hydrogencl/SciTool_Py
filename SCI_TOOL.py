a3f248994f88e6d69787c92a519aa9e80f17d10f
MTP84 <metalpen1984@users.noreply.github.com>
2020-05-12 12:40:50 +0200

    Update SCI_TOOL.py
    Adding a function to calculate the loop progress

--- SCI_TOOL.py
+++ SCI_TOOL.py
@@ -8,3 +8,11 @@ def progress_bar(NUM_PROGRESS, NUM_PROGRESS_BIN=0.2, STR_SYS_SYMBOL="=", STR_DES
     sys.stdout.write('[{0:20s}\>]{1:4.1f}% {2:s}'.format(STR_SYS_SYMBOL*NUM_SYM, NUM_PROGRESS*100, STR_DES))
     sys.stdout.flush()
     
+def loop_progress_cal(ARR_INDEX, ARR_INDEX_MAX, NUM_OUT=1, NUM_ERROR=-9999.999):
+    if len(ARR_INDEX) == len(ARR_INDEX_MAX):
+        for i, i_index in enumerate(ARR_INDEX):
+            NUM_OUT = NUM_OUT * (i_index/float(ARR_INDEX_MAX[i])) 
+        return NUM_OUT
+    else:
+        print("Wrong dimenstion for in put ARR_INDEX ({0:d}) and ARR_INDEX_MAX ({1:d})".format(len(ARR_INDEX), len(ARR_INDEX_MAX)))
+        return NUM_ERROR
