7dce1066a99cbc6289d0d56f1f291f634d1d33b7
ye.lu <ye.lu@fz-juelich.de>
2021-05-20 09:47:12 +0200

    GRIDINFORMER.py Adding the timestamp function

--- GRIDINFORMER.py
+++ GRIDINFORMER.py
@@ -685,12 +685,17 @@ class MATH_TOOLS:
 
 class TOOLS:
     """ TOOLS is contains:
+        timestamp
         fix_ind
         progress_bar
         cal_progrss
     """
     ARR_HOY      = [0, 744, 1416, 2160, 2880, 3624, 4344, 5088, 5832, 6552, 7296, 8016, 8760] 
     ARR_HOY_LEAP = [0, 744, 1440, 2184, 2904, 3648, 4368, 5112, 5856, 6576, 7320, 8040, 8784]
+
+    def timestamp(STR_IN=""):
+        print("{0:04d}-{1:02d}-{2:02d}_{3:02d}:{4:02d}:{5:02d} {6:s}".format(time.gmtime().tm_year, time.gmtime().tm_mon, time.gmtime().tm_mday,\
+        time.gmtime().tm_hour, time.gmtime().tm_min, time.gmtime().tm_sec, STR_IN) )
 
     def fix_ind(IND_IN, IND_J, IND_I, ARR_XRANGE=[], ARR_YRANGE=[], NX=0, NY=0):
         NUM_DY   = ARR_YRANGE[0]
