57e6bb07c30c7f27eec750d279333c67bc5fffc8
ye.lu <ye.lu@fz-juelich.de>
2023-04-28 11:33:31 +0200

    adding the function of run_time_cal

--- GRIDINFORMER.py
+++ GRIDINFORMER.py
@@ -951,6 +951,20 @@ class TOOLS:
         else:
             print("Wrong dimenstion for in put ARR_INDEX ({0:d}) and ARR_INDEX_MAX ({1:d})".format(len(ARR_INDEX), len(ARR_INDEX_MAX)))
 
+    def run_time_cal(ARR_TIME_IN, IF_LEAP=False, NUM_MON=0):
+        if IF_LEAP == True:
+            ARR_DAY_LIM = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
+        else:
+            ARR_DAY_LIM = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
+        if NUM_MON == 0:
+            DAYS    = ARR_TIME_IN[0] * sum(ARR_DAY_LIM) + ARR_DAY_LIM[ ARR_TIME_IN[1] ] + ARR_TIME_IN[2]
+        else:
+            DAYS    = ARR_TIME_IN[0] * sum(ARR_DAY_LIM) + ARR_TIME_IN[1] * NUM_MON + ARR_TIME_IN[2]
+        HOURS   = DAYS * 24 + ARR_TIME_IN[3]
+        MINUTES = HOURS * 60 + ARR_TIME_IN[4]
+        return {"DAYS": DAYS, "HOURS": HOURS, "MINUTES": MINUTES }
+
+
     def calendar_cal(ARR_START_TIME, ARR_INTERVAL, ARR_END_TIME_IN=[0, 0, 0, 0, 0, 0.0], IF_LEAP=False):
         ARR_END_TIME  = [ 0,0,0,0,0,0.0]
         ARR_DATETIME  = ["SECOND", "MINUTE", "HOUR","DAY", "MON", "YEAR"]
